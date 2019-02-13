426f81c9b42a0cec0e6276fdc08029a768319ef4
tjtanjin <cjtanjin@gmail.com>
2019-02-13 11:12:51 +0800

    Added comments. Removed some unnecessary codes. Added ability to identify and bound multiple texts instead of one.

--- text_identifier.py
+++ text_identifier.py
@@ -1,40 +1,49 @@
 import pytesseract, cv2
 from pytesseract import Output
 
+#read and double image size for increased accuracy
 img = cv2.imread(image_name)
 img = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 
+#read data from image
 d = pytesseract.image_to_data(img, output_type=Output.DICT)
-n_boxes = len(d['level'])
 
-test = [text_to_look_for]
-newtest =  [word for line in test for word in line.split()]
+#string to identify (separate multiple sentences with commas)
+identify_string = [text_to_look_for_1, text_to_look_for_2]
+
+#create an initially empty list to hold coordinates and set initial box with to 65
 coordinates = []
 width = 65
-count = 0
 
-for words in newtest:
-    for y in d["text"]:
-        if words == y:
-            if coordinates != []:
-                order = d["text"].index(words) - previous_index
-                if order == 1 or order == -1:
+#repeat for every sentence in identify_string
+for sentence in identify_string:
+    sentence = [sentence]
+    newtest = [word for line in sentence for word in line.split()]
+    for words in newtest:
+        for y in d["text"]:
+            #check if words match
+            if words == y:
+                if coordinates != []:
+                    order = d["text"].index(words) - previous_index
+                    #append the coordinate only if they are within 1 index to ensure they are from the same sentence
+                    if order == 1 or order == -1:
+                        coordinates.append(d["text"].index(words))
+                        previous_index = d["text"].index(words)
+                    else:
+                        d["text"][d["text"].index(words)] = ""
+                else:
                     coordinates.append(d["text"].index(words))
                     previous_index = d["text"].index(words)
-                else:
-                    d["text"][d["text"].index(words)] = ""
-            else:
-                coordinates.append(d["text"].index(words))
-                previous_index = d["text"].index(words)
-
-for i in range(len(coordinates)):
-            width += d["width"][coordinates[i]]
-print(coordinates)
-left = d["left"][coordinates[0]] - 15
-top = d["top"][coordinates[0]] - 10
-height = d["height"][coordinates[0]] + 20
-for i in range(n_boxes):
+    #modify width for bounding box
+    for i in range(len(coordinates)):
+                width += d["width"][coordinates[i]]
+    #print(coordinates) #uncomment to view coordinates of box drawn
+    left = d["left"][coordinates[0]] - 15
+    top = d["top"][coordinates[0]] - 10
+    height = d["height"][coordinates[0]] + 20
     (x, y, w, h) = (left, top, width, height)
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    coordinates = []
 
+#save image
 cv2.imwrite(saved_image_name,img)
